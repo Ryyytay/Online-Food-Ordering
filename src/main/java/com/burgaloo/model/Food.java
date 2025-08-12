a31cc3ccaf6e57371bcb95ba23b354a824a37c81
Ryyytay <taypeng1214@gmail.com>
2025-08-12 19:00:29 +1200

    Update

--- src/main/java/com/burgaloo/model/Food.java
+++ src/main/java/com/burgaloo/model/Food.java
@@ -36,8 +36,9 @@ public class Food {
 
     private boolean isSeasonal;
 
+    @ManyToMany
     private List<IngredientsItem> ingredients = new ArrayList<>();
-
-
+    
+    private Date creationDate;
 
 }
---
+++ src/main/java/com/burgaloo/model/IngredientsItem.java
@@ -0,0 +1,20 @@
+package com.burgaloo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientsItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
