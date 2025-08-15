ba455b932dac9e709f0f849db9a688d29cbb3c2f
Ryyytay <taypeng1214@gmail.com>
2025-08-15 20:20:36 +1200

    Update

---
+++ src/main/java/com/burgaloo/model/Cart.java
@@ -0,0 +1,29 @@
+package com.burgaloo.model;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User customer;
+
+    private Long total;
+
+    @OneToMany(mappedBy = "cart", cascade = Cascade.All, orphanRemoval = true)
+    private List<CartItem> item = new ArrayList<>();
+}
