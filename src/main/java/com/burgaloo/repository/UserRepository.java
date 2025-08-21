76e806d1142bb48b9676844f6ef9a2935d2a6afc
Ryyytay <taypeng1214@gmail.com>
2025-08-21 19:28:07 +1200

    config app setting

---
+++ src/main/java/com/burgaloo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.burgaloo.repository;
+
+import com.burgaloo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByEmail(String username);
+
+
+
+}
