05801fd204924be8e4d6632b33749e9dc8c8ef32
Ryyytay <taypeng1214@gmail.com>
2025-09-16 20:21:22 +1200

    Update

--- src/main/java/com/burgaloo/controller/AdminFoodController.java
+++ src/main/java/com/burgaloo/controller/AdminFoodController.java
@@ -4,15 +4,14 @@ import com.burgaloo.model.Food;
 import com.burgaloo.model.Restaurant;
 import com.burgaloo.model.User;
 import com.burgaloo.request.CreateFoodRequest;
+import com.burgaloo.response.MessageResponse;
 import com.burgaloo.service.FoodService;
 import com.burgaloo.service.RestaurantService;
 import com.burgaloo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/food")
@@ -27,11 +26,37 @@ public class AdminFoodController {
     @Autowired
     private RestaurantService restaurantService;
 
+    @PostMapping
     public ResponseEntity<Food> createFood(@RequestBody CreateFoodRequest req,
                                            @RequestHeader("Authorization") String jwt) throws Exception{
         User user = userService.findUserByJwtToken(jwt);
         Restaurant restaurant = restaurantService.findRestaurantById(req.getRestaurantId());
-        return null;
+        Food food = foodService.createFood(req, req.getCategory(), restaurant);
+
+        return new ResponseEntity<>(food, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<MessageResponse> deleteFood(@PathVariable Long id,
+                                           @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserByJwtToken(jwt);
+
+        foodService.deleteFood(id);
+
+        MessageResponse res = new MessageResponse();
+        res.setMessage("Food deleted successfully");
+
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Food> updateFoodAvailabilityStatus(@PathVariable Long id,
+                                                      @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserByJwtToken(jwt);
+
+        Food food = foodService.updateAvailabilityStatus(id);
+
+        return new ResponseEntity<>(food, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/burgaloo/controller/FoodController.java
+++ src/main/java/com/burgaloo/controller/FoodController.java
@@ -1,10 +1,38 @@
 package com.burgaloo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.burgaloo.model.Food;
+import com.burgaloo.model.Restaurant;
+import com.burgaloo.model.User;
+import com.burgaloo.request.CreateFoodRequest;
+import com.burgaloo.service.FoodService;
+import com.burgaloo.service.RestaurantService;
+import com.burgaloo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/food")
 public class FoodController {
+    @Autowired
+    private FoodService foodService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Food>> searchFood(@RequestParam String name,
+                                           @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserByJwtToken(jwt);
+
+        List<Food> foods = foodService.searchFood(name);
+
+        return new ResponseEntity<>(foods, HttpStatus.CREATED);
+    }
 }
