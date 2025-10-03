24b703949d39c4fd02b2b16b52f6feda2716dad2
Ryyytay <taypeng1214@gmail.com>
2025-10-04 11:46:02 +1300

    Update Home Page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -5,12 +5,14 @@ import { Navbar } from './component/Navbar/Navbar'
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 import { darkTheme } from './Theme/DarkTheme'
+import Home from './component/Home/Home'
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar/>
+      <Home/>
     </ThemeProvider>
   )
 }
