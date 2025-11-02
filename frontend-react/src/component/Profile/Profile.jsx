01f9e4bc34d07c54690adb8c48e13a63eb7ff99c
Ryyytay <taypeng1214@gmail.com>
2025-11-02 14:36:24 +1300

    Update routes

--- frontend-react/src/component/Profile/Profile.jsx
+++ frontend-react/src/component/Profile/Profile.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import ProfileNavigation from "./ProfileNavigation";
 
 export default function Profile() {
+    const [openSideBar, setOpenSideBar] = useState(false);
+
   return (
     <div className="lg:flex justify-between">
         <div className="sticky h-[80vh] lg:w-[20%]">
-            <ProfileNavigation/>
+            <ProfileNavigation open={openSideBar}/>
         </div>
         <div className="lg:w-[80%]">
 
--- frontend-react/src/component/Profile/ProfileNavigation.jsx
+++ frontend-react/src/component/Profile/ProfileNavigation.jsx
@@ -6,6 +6,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import EventIcon from '@mui/icons-material/Event';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Divider, Drawer, useMediaQuery } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const menu = [
     {title:"Orders", icon:<ShoppingBagIcon/>},
@@ -18,20 +19,24 @@ const menu = [
 ]
 
 export default function ProfileNavigation({open, handleClose}) {
-    const isSmallScreen = useMediaQuery("(max-width:1080)");
+    const isSmallScreen = useMediaQuery('(max-width:900px)');
+    const navigate = useNavigate(); 
+    const handleNavigate = (item) => {
+        navigate(`/my-profile/${item.title.toLowerCase()}`)
+    }
 
-  return (
+  return ( 
     <div>
         <Drawer 
-            variant ={isSmallScreen?"temporary":"permanent"} 
+            variant ={isSmallScreen? "temporary" : "permanent"} 
             onClose={handleClose} 
-            open={open} 
+            open={isSmallScreen? open : true} 
             anchor='left' 
-            sx={{zIndex:1}}
+            sx={{ zIndex : -1, position:"sticky"}}
         >
-            <div className="w-[50vw] lg:w-[20vw] h-[100vh] flex flex-col justify-center text-xl gap-8 pt-16">
+            <div onClick={handleNavigate} className="w-[50vw] lg:w-[20vw] h-[100vh] flex flex-col justify-center text-xl pt-16 gap-8">
                 {menu.map((item, i) => <>
-                    <div className='px-5 flex items-center space-x-5 cursor-pointer'>
+                    <div onClick={() => handleNavigate(item)} className='px-5 flex items-center space-x-5 cursor-pointer'>
                         {item.icon}
                         <span>{item.title}</span>
                     </div>
